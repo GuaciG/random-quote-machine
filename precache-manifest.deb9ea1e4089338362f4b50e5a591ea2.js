5533f3450a31a6e3614eef1e75560a9f555862a1
Guaci Gutierrez <guacig@hotmail.com>
2020-10-23 13:07:03 +0100

    Updates

---
+++ precache-manifest.deb9ea1e4089338362f4b50e5a591ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a5e83b4978dcb5caf66519ed9d6e03",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e6c90c561900a2ff23db",
+    "url": "/random-quote-machine/static/css/main.6975ac02.chunk.css"
+  },
+  {
+    "revision": "2b96a2b976ca76ba73c4",
+    "url": "/random-quote-machine/static/js/2.4342779c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/random-quote-machine/static/js/2.4342779c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c90c561900a2ff23db",
+    "url": "/random-quote-machine/static/js/main.068d37c5.chunk.js"
+  },
+  {
+    "revision": "8606d7d9ddc9f477af7c",
+    "url": "/random-quote-machine/static/js/runtime-main.10e463cd.js"
+  }
+]);
